2f570f679e6200e5464b141f3cf639763b1c6add
LarisPauletic <58471911+LarisPauletic@users.noreply.github.com>
2021-01-17 14:04:22 +0100

    Add files via upload

---
+++ 6.Zadatak/6.Zadatak/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Zadatak
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int i;
+            int rez=0;
+            Console.Write("Upisite broj do kojeg ce se zbrajati: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (i = 0; i <= n; i++)
+            {
+               rez +=i;
+                
+            }
+            Console.Write("Zbroj prvih ");
+            Console.Write(n);
+            Console.Write(" brojeva je: ");
+            Console.Write(rez);
+            Console.ReadKey();
+        
+        }
+
+    }
+}
